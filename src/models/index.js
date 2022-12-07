a62595d5b43c9339c433d46952aa06d7064cad3e
alidmrcn <ahdemircan@outlook.com>
2022-12-07 14:24:35 +0000

    rename and update

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,7 @@
 const Employee = require('./employee')
 const Shift = require('./shift')
+const Incident = require('./incident')
+const Contract = require('./contract')
 
 const Ali = new Employee('Ali', 'MEC')
 const Selim = new Employee('Selim', 'ELC')
@@ -8,6 +10,10 @@ const Kumar = new Employee('Kumar', 'ICT')
 const One = new Shift(1, '15/11/2022', 'Day')
 const Two = new Shift(2, '15/11/2022', 'Night')
 const Four = new Shift(4, '17/11/2022', 'Night')
+
+const Incident01 = new Incident('FIDS', 'Black Screen', 'Checkin 46')
+const HoneywellOncallContract = new Contract('Oncall Contract', 'Honeywell', '2021/10/05', '2021/11/01', '2022/11/01/')
+// write consolelog
 
 Ali.addShift(One)
 Ali.addShift(Two)
