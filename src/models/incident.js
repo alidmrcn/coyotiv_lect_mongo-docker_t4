a89f9498d737b95b65e4470562092bf053e0352b
alidmrcn <ahdemircan@outlook.com>
2022-12-07 14:23:37 +0000

    update class properties

--- src/models/incident.js
+++ src/models/incident.js
@@ -1,11 +1,41 @@
 class Incident {
-  constructor(system, reportDescription, location) {
+  constructor(system, noticeDescription, location) {
     this.system = system
-    this.reportDescription = reportDescription
+    this.noticeDescription = noticeDescription
     this.location = location
-    this.startDate = ''
-    this.leaveDate = ''
+    this.noticeBy = '' // ? Shall I add by constructor or function
+    this.noticeDate = '' // ? How shall I arrange date
+    this.noticeTime = '' // ? How shall I arrange time
+    this.responseDate = ''
+    this.responseTime = ''
+    this.incidentType = ''
+    this.incidentDescription = ''
+    this.incidentPhoto = []
+    this.resolutionDate = ''
+    this.resolutionTime = ''
+    this.resolutionPhoto = []
+    this.resolutionDescription = ''
+    this.incidentDuration = '' // ! Function is to be added for calculation
+    this.rootCause = ''
+    this.usedMaterials = []
+    this.involvedEmployee = []
+    this.spendtManHour = '' // ! Function is to be added for calculation
+    this.reportFilledBy = ''
+    this.reportDate = ''
+    this.reportTime = ''
+    this.reportNo = ''
+  }
+
+  addIncidentPhoto(photo) {
+    this.incidentPhoto.push(photo)
   }
 }
 
+// add other functions
+
 module.exports = Incident
+
+// to be added later:
+// - status
+// - improvement actions and plan
+// - 3rd party involment details
