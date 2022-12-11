7420c262a8553a3df788ceb695aa903b99f05ae5
alidmrcn <ahdemircan@outlook.com>
2022-12-11 14:43:01 +0000

    create route files

--- src/routes/departments.js
+++ src/routes/departments.js
@@ -1,19 +1,19 @@
 const express = require('express')
-const Shift = require('../models/shift') // + user'i shift yaptim
+const Department = require('../models/department')
 
 const router = express.Router()
 
-/* GET shifts listing. */
+/* GET departments listing. */
 router.get('/', async (req, res) => {
-  res.send(await Shift.find({}).catch(error => console.log('Shifts not found, error: ', error)))
+  res.send(await Department.find({}).catch(error => console.log('Departments not found, error: ', error)))
 })
 
 /* GET initialize */
 router.get('/initialize', async (req, res) => {
-  const one = await Shift.create({ shiftno: 1, day: '15/11/2022', period: 'Day' })
+  // const one = await Shift.create({ shiftno: 1, day: '15/11/2022', period: 'Day' }) // ! Add new
   // const Two = await Shift.create({ shiftno: 2, day: '15/11/2022', period: 'Night' })
   // const Four = await Shift.create({ shiftno: 4, day: '17/11/2022', period: 'Night' })
-  res.send(one)
+  // res.send(one)
 })
 
 /* POST user */
